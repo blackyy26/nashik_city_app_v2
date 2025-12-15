fc884e6f0cc47848a8f1f0a77dd763e69c82304c
blackyy26 <deshmukhrahul9022@gmail.com>
2025-12-15 13:03:40 +0530

    Update Fix home screen UI and bracket issues

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -31,39 +31,44 @@ class _HomeItem extends StatelessWidget {
   final String title;
   final IconData icon;
 
-  const _HomeItem({required this.title, required this.icon});
+  const _HomeItem({
+    required this.title,
+    required this.icon,
+  });
 
   @override
   Widget build(BuildContext context) {
-   return InkWell(
-  onTap: () {
-    if (title == 'Emergency') {
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => const EmergencyScreen(),
+    return InkWell(
+      onTap: () {
+        if (title == 'Emergency') {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const EmergencyScreen(),
+            ),
+          );
+        }
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.blue.shade50,
+          borderRadius: BorderRadius.circular(20),
         ),
-      );
-    }
-  },
-  child: Container(
-    decoration: BoxDecoration(
-      color: Colors.blue.shade50,
-      borderRadius: BorderRadius.circular(20),
-    ),
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(icon, size: 50, color: Colors.blue),
-        const SizedBox(height: 10),
-        Text(
-          title,
-          style: const TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(icon, size: 50, color: Colors.blue),
+            const SizedBox(height: 10),
+            Text(
+              title,
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
         ),
-      ],
-    ),
-  ),
-);
+      ),
+    );
+  }
+}
