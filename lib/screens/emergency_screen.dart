9229afeaad2d24992a708423ecdb3ac896084497
blackyy26 <deshmukhrahul9022@gmail.com>
2025-12-17 21:02:38 +0530

    Update emergency_screen.dart

--- lib/screens/emergency_screen.dart
+++ lib/screens/emergency_screen.dart
@@ -7,13 +7,13 @@ class EmergencyScreen extends StatelessWidget {
   Future<void> _callNumber(String number) async {
     final Uri uri = Uri(scheme: 'tel', path: number);
 
-    try {
+    if (await canLaunchUrl(uri)) {
       await launchUrl(
         uri,
         mode: LaunchMode.externalApplication,
       );
-    } catch (e) {
-      debugPrint('Call failed: $e');
+    } else {
+      debugPrint('Could not launch dialer');
     }
   }
 
@@ -30,21 +30,4 @@ class EmergencyScreen extends StatelessWidget {
           _tile('Police', '100', Icons.local_police),
           _tile('Ambulance', '108', Icons.local_hospital),
           _tile('Fire Brigade', '101', Icons.fire_truck),
-          _tile('Women Helpline', '181', Icons.woman),
-        ],
-      ),
-    );
-  }
-
-  Widget _tile(String title, String number, IconData icon) {
-    return Card(
-      child: ListTile(
-        leading: Icon(icon, color: Colors.red),
-        title: Text(title),
-        subtitle: Text('Call $number'),
-        trailing: const Icon(Icons.call),
-        onTap: () => _callNumber(number),
-      ),
-    );
-  }
-}
+          _tile('Women Helpline
