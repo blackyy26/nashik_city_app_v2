c770080478ecd6ae9cc68aa6232916d254dda6fa
blackyy26 <deshmukhrahul9022@gmail.com>
2025-12-17 20:47:12 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,23 @@
 import 'package:flutter/material.dart';
-import 'screens/login_screen.dart';
+import 'screens/home_screen.dart';
 
 void main() {
-  runApp(const NashikApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
 }
 
-class NashikApp extends StatelessWidget {
-  const NashikApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Nashik City App',
-      home: const LoginScreen(),
+      title: 'Navigation and Routing',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
     );
   }
 }
