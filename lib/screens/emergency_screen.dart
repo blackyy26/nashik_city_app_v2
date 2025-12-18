ee71f31f2302dd2e6dec0d6e204b90c1d10d42c1
blackyy26 <deshmukhrahul9022@gmail.com>
2025-12-18 11:41:14 +0530

    Update emergency_screen.dart

--- lib/screens/emergency_screen.dart
+++ lib/screens/emergency_screen.dart
@@ -30,4 +30,21 @@ class EmergencyScreen extends StatelessWidget {
           _tile('Police', '100', Icons.local_police),
           _tile('Ambulance', '108', Icons.local_hospital),
           _tile('Fire Brigade', '101', Icons.fire_truck),
-          _tile('Women Helpline
+          _tile('Women Helpline', '181', Icons.woman),
+        ],
+      ),
+    );
+  }
+
+  Widget _tile(String title, String number, IconData icon) {
+    return Card(
+      child: ListTile(
+        leading: Icon(icon, color: Colors.red),
+        title: Text(title),
+        subtitle: Text('Call $number'),
+        trailing: const Icon(Icons.call),
+        onTap: () => _callNumber(number),
+      ),
+    );
+  }
+}
